80b0e0421054b2407f890c512a165a1202da9f37
mistiadhikari <mistichettri815@gmail.com>
2026-07-25 21:44:33 +0545

    assignment

--- day4.py
+++ day4.py
@@ -49,10 +49,34 @@
 
 
 
-def c_to_f(c):
-    return c*1.8+32
-print(c_to_f(10))
-def f_to_c(f):
-    return f-32/1.8
-print(f_to_c(109))
+# def c_to_f(c):
+#     return c*1.8+32
+# print(c_to_f(10))
+# def f_to_c(f):
+#     return f-32/1.8
+# print(f_to_c(109))
 
+
+
+#assignment
+def student_marksheet():
+    student_name = input("Enter student name: ")
+
+    subject1 = float(input("Enter marks in English: "))
+    subject2 = float(input("Enter marks in Mathematics: "))
+    subject3 = float(input("Enter marks in Science: "))
+    subject4 = float(input("Enter marks in Computer: "))
+
+    total = subject1 + subject2 + subject3 + subject4
+    percentage = total / 4
+
+    print("\nSTUDENT MARKSHEET")
+    print("Student Name:", student_name)
+    print("English:", subject1)
+    print("Mathematics:", subject2)
+    print("Science:", subject3)
+    print("Computer:", subject4)
+    print("Total Marks:", total)
+    print("Percentage:", percentage, "%")
+
+student_marksheet()# Calling the function
