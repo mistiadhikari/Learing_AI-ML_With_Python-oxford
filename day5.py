83c31aa3cbf86dfb25819cd51b89be198b99c322
mistiadhikari <mistichettri815@gmail.com>
2026-07-27 10:07:35 +0545

    day5

--- day5.py
+++ day5.py
@@ -61,9 +61,70 @@
 
 #real world use of **kwargs
 
-def employee(**details):
-    for key,value in details.items():
-        print(key,"=",value)
+# def employee(**details):
+#     for key,value in details.items():
+#         print(key,"=",value)
 
-employee(Name="misti", department="it",salary= 400000, country="USA")
-    
+# employee(Name="misti", department="it",salary= 400000, country="USA")
+
+
+
+#lambda function
+# def square(x):
+#     return x*x
+# print(square(6))
+
+
+# square= lambda x:x*x#lambda
+# print(square(5))
+
+
+# multiply= lambda x,y : x*y
+# print(multiply(4,3))
+
+
+# def calculate_salary(hours,rate):
+#     salary= hours*rate
+#     tax= salary*0.1
+#     final_salary = salary-tax
+#     return final_salary
+# print(calculate_salary(9,200)) #cannot be used in lambda
+
+
+
+
+#sorting data using lambda
+
+# student= [
+#     ("ram",78),
+#     ("hari",92),
+#     ("sita", 85)
+#  ]
+# student.sort(key= lambda student:student[1]) #sorting using marks
+# print(student)
+
+
+#lambda with map()
+
+# marks= [50,60,70,80]
+# new_marks=[]
+# for mark in marks:
+#     new_marks.append(mark+5)
+# print(new_marks)
+
+
+# new_markks=list(map(lambda x:x+5,marks))
+# print(new_markks)
+
+
+#filtering'''
+# marks= [50,60,70,80,67,32,54,90,100]
+# passed=list(filter(lambda x:x>60, marks))
+
+# print(passed)
+
+
+from functools import reduce
+numbers= [10,20,30,40]
+total = reduce(lambda x,y:x+y,numbers) #reduce takes 2 numbers and give their sum
+print( total)
